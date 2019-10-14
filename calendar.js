38d0667af09945f5220914bd145637a29ad1e68e
phil-harmston <phillipharmston@yahoo.com>
2019-10-13 19:58:28 -0600

    added functionality and changed quite a bit of code.

--- calendar.js
+++ calendar.js
@@ -1,41 +1,105 @@
+// this is the object constructor for the tiles
+// representing the days of the month
 var Tile = function(x,y){
     this.x = x;
-    this.y =y;
+    this.y = y;
     this.width = 100;
+    this.today = "";
+    this.event = "";
+    
 }
 
-
+// This is the object constructor for the days of the week at the
+// top of the calendar
 var DayOfWeek = function(x,y){
     this.x = x;
     this.y = y;
     this.width = 100;
     this.height = 20;
 }
+// empty array of tiles
+let tiles = [];
 
-var my_date = new Date();
-var day_of_week = my_date.getDay()
-var firstDay = new Date(my_date.getFullYear(), my_date.getMonth(), 1);
-var lastDay = new Date(my_date.getFullYear(), my_date.getMonth()+1, 0);
+// track mouse clicks
+let mouse_click_count = 0;
 
-Tile.prototype.draw = function(){
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.strokeStyle="red";
-    ctx.rect(this.x,this.y,this.width, this.width);
+// Build some date vars for use later
+let my_date = new Date();
+let day_of_week = my_date.getDay()
+
+// This returns the first day of given month.
+let firstDay = new Date(my_date.getFullYear(), my_date.getMonth(), 1);
+
+// lastDay is the last day of the month
+let lastDay = new Date(my_date.getFullYear(), my_date.getMonth()+1, 0);
+
+// this_month is the current month
+let this_month = my_date.getMonth() + 1;
+
+// this year is the current year
+let this_year = my_date.getYear() + 1900;
+
+//console.log("month: " + this_month + " year: " + this_year)
+//console.log("Days in this month:"  + daysInMonth(this_month, this_year))
+
+
+//function to return the number of days in a given month.
+function daysInMonth(month, year){
+    return new Date(year, month, 0).getDate();  
+}
+
+// function iterates through tiles[] setting the date on each day so it can be drawn
+function setDaysInMonth(){
+   i=0;
+    for(let x = firstDay.getDate(); i <= daysInMonth(this_month, this_year); x++){
+       tiles[x].today = i;
+       i++;
+   }
+
+}
+
+
+//console.log(day_of_week)
+
+// Draws the calendar
+Tile.prototype.draw = function(x, y, i){
+    // if statement draws a different color for days not in month
+    if (this.today ==""){
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.strokeStyle="green";
+        this.today = "";
+        ctx.rect(this.x,this.y,this.width, this.width);
+   
+    }else{
+    //console.log(this.today);
+    
+    // draws all other days in month a different color
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.beginPath();
+        ctx.strokeStyle="black";
+        ctx.font = "24px Arial";
+        
+        ctx.fillText(this.today, x+70, y+20);
+        ctx.rect(this.x,this.y,this.width, this.width);
+    }     
     ctx.stroke();   
 }
 
-Tile.prototype.changeColor = function(){
+// function is called on mouse click needs to be completed
+Tile.prototype.changeColor = function(color){
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.beginPath();
-    ctx.strokeStyle="green";
+    ctx.strokeStyle = color;
     ctx.rect(this.x,this.y,this.width, this.width);
     ctx.stroke();
-    
 }
 
+// function takes three params posx, posy, index used by case to fill the
+// correct day.
 DayOfWeek.prototype.draw = function(x ,y, index){
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
@@ -60,25 +124,24 @@ DayOfWeek.prototype.draw = function(x ,y, index){
             ctx.fillText("Friday", x, y); 
             break;
         case 6:
-            ctx.fillText("Saturday", x, y); 
-            
-    }
-    
-    
+            ctx.fillText("Saturday", x, y);        
+    }    
 }
 
-console.log(day_of_week)
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
-var arr_Days =[];
- var hcols = 7;
-    var hrows = 1;
+// This block of code puts the days above the columns of our calendar
+let arr_Days =[];
+let hcols = 7;
+let hrows = 1;
 
-for (var i = 0; i< hcols; i++){
+// var j needs to be global to draw the board
+for (let i = 0; i< hcols; i++){
     for (var j = 0; j < hrows; j++){
-        var headerx = i * 104 + 20;
-        var headery = j * 104 + 40;
+        let headerx = i * 104 + 20;
+        let headery = j * 104 + 40;
         arr_Days.push(new DayOfWeek(headerx, headery, this.width, this.width))
     }
 }
@@ -86,25 +149,68 @@ for (var i = 0; i< hcols; i++){
 
 for (var i =0; i < arr_Days.length; i++){
     arr_Days[i].draw(arr_Days[i].x, arr_Days[i].y, i);
-    console.log(this.x)
+    
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
-  var tiles = [];
-    var cols = 7;
-    var rows = 6;
 
-    for (var i=0; i < cols; i++){
-        for (var j = 0; j < rows; j++){
-            var tilex = i * 104 +5;
-            var tiley = j * 104 +40;
-            tiles.push(new Tile(tilex, tiley, this.width, this.width));
+let cols = 4;
+let rows = 6;
+
+/*
+rather than drawing the whole array at once we create one week at a time so the array is in order
+
+*/
+    for (let k = 0; k <= cols; k++){
+        if (k == 0){
+            for (let i=0; i <= rows; i++){
+                let tilex = i * 103;
+                let tiley = j * 50; // off set work
+                tiles.push(new Tile(tilex, tiley, this.width, this.width));    
+            }
         }
-    }
+/*
+creates week 2
+*/
+        if(k == 1){
+            for (i =0; i <= rows; i++) {
+                var tilex = i * 103;
+                var tiley = j * 203-50;
+                tiles.push(new Tile(tilex, tiley, this.width, this.width));
+            }
+        }
+      
+        if(k == 2){
+            for (i =0; i <= rows; i++) {
+                var tilex = i * 103;
+                var tiley = j * 303-47;
+                tiles.push(new Tile(tilex, tiley, this.width, this.width));
+            }   
+        } 
+        if(k == 3){
+            for (i =0; i <= rows; i++) {
+                var tilex = i * 103;
+                var tiley = j * 403-44;
+                tiles.push(new Tile(tilex, tiley, this.width, this.width));
+            }   
+        } 
+        if(k == 4){
+            for (i =0; i <= rows; i++) {
 
-    for (var i =0; i < tiles.length; i++){
-    tiles[i].draw();
+                var tilex = i * 103;
+                var tiley = j * 503-41;
+                tiles.push(new Tile(tilex, tiley, this.width, this.width));
+
+            }   
+        } 
+    }//main for loop
+    
+// now we can draw the array passing i to the draw function to see if the day is in the
+// current month.
+    setDaysInMonth();
+    for (var i = 0; i < tiles.length; i++){
+    tiles[i].draw(tiles[i].x, tiles[i].y, i);
     }
 //////////////////////////////////////////////////////////////////////////////////////
 
@@ -113,7 +219,7 @@ for (var i =0; i < arr_Days.length; i++){
 
 
 
-function openForm(){
+function Event_Scheduler_Menu(){
     document.getElementById("myForm").style.display = "block";
 }
 
@@ -128,7 +234,7 @@ Tile.prototype.isUnderMouse = function(x,y){
 //report the mouse position on click
 myCanvas.addEventListener("click", function (evt) {
     var mousePos = getMousePos(myCanvas, evt);
-    console.log(mousePos.x + ',' + mousePos.y);
+    //console.log(mousePos.x + ',' + mousePos.y);
     mouseClicked(mousePos.x, mousePos.y)
 }, false);
 
@@ -141,13 +247,30 @@ function getMousePos(canvas, evt){
     }
 }
 
-mouseClicked = function(mouseX, mouseY) {
+
+function mouseClicked(mouseX, mouseY) {
     // check if mouse was inside a tile
-    for (var i = 0; i < tiles.length; i++) {
-        if (tiles[i].isUnderMouse(mouseX, mouseY)) {
-        tiles[i].changeColor();
-        openForm();
-        
+    if(mouse_click_count == 0){
+       Event_Scheduler_Menu(); 
+      
+        for (let i = 0; i < tiles.length; i++) {
+            if(tiles[i].isUnderMouse(mouseX, mouseY)) {
+                let start_day = tiles[i].today;
+                document.getElementById("start_date").value =  this_month + "/" + start_day + "/" + this_year;
+                tiles[i].changeColor("red");
+                mouse_click_count = 1;  
+            }
         }
+    }else{   
+        for (let i = 0; i < tiles.length; i++) {
+            if (tiles[i].isUnderMouse(mouseX, mouseY)) {
+                let start_day = tiles[i].today;
+                document.getElementById("end_date").value =  this_month + "/" + start_day + "/" + this_year;
+                tiles[i].changeColor("blue");
+                mouse_click_count = 0;      
+            }
+        }  
     }
-}
+   
+    }
+
